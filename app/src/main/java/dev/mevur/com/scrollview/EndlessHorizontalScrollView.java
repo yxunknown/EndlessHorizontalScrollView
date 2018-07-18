eb17673592a8f1228e8137f4b71992c8f70df254
mevur <cplzwr@live.cn>
2018-07-18 16:36:38 +0800

    endless scrollview with gyroscope sensor

--- app/src/main/java/dev/mevur/com/scrollview/EndlessHorizontalScrollView.java
+++ app/src/main/java/dev/mevur/com/scrollview/EndlessHorizontalScrollView.java
@@ -1,17 +1,34 @@
 package dev.mevur.com.scrollview;
 
 import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.HorizontalScrollView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
-public class EndlessHorizontalScrollView extends HorizontalScrollView {
+public class EndlessHorizontalScrollView extends HorizontalScrollView
+        implements SensorEventListener{
+    private static final float NANO_SECOND_TO_SECOND = 1.0F / 1000000000.0F;
+    private static final int MAGINIFY_FACTOR = 1000;
     private Context context;
     private int actualScrollRange;
     private int restScrollRange;
+    private Sensor gyroScopeSensor;
+    private SensorManager sensorManager;
+    private long timestamp;
+    private float angelx;
+    private float angely;
+    private float angelz;
+    private int pixelsPerDegree;
+
+
 
     public EndlessHorizontalScrollView(Context context) {
         super(context, null);
@@ -32,7 +49,17 @@ public class EndlessHorizontalScrollView extends HorizontalScrollView {
         this.context = context;
         setEnabled(false);
         setOverScrollMode(OVER_SCROLL_NEVER);
+        sensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
+        gyroScopeSensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        sensorManager.registerListener(this, gyroScopeSensor, SensorManager.SENSOR_DELAY_GAME);
     }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        //disable touch to scroll
+        return false;
+    }
+
 
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
@@ -55,6 +82,7 @@ public class EndlessHorizontalScrollView extends HorizontalScrollView {
     @Override
     protected void onOverScrolled(int scrollX, int scrollY, boolean clampedX, boolean clampedY) {
       super.onOverScrolled(scrollX, scrollY, clampedX, clampedY);
+      //implement endless scroll
 //        restScrollRange = actualScrollRange - scrollX;
         LinearLayout container = (LinearLayout) getChildAt(0);
 //        System.out.printf("%4d %4d\n", restScrollRange, scrollX);
@@ -72,5 +100,61 @@ public class EndlessHorizontalScrollView extends HorizontalScrollView {
     protected void onScrollChanged(int l, int t, int oldl, int oldt) {
         super.onScrollChanged(l, t, oldl, oldt);
 //        System.out.println("l = [" + l + "], t = [" + t + "], oldl = [" + oldl + "], oldt = [" + oldt + "]");
+    }
+
+    public int getMaxScroll() {
+        return computeHorizontalScrollRange();
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        //there is where the view die
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        pixelsPerDegree = computeHorizontalScrollRange() / 360;
+        if (Sensor.TYPE_GYROSCOPE == event.sensor.getType()) {
+            //handle gyroscope sensor data here
+            if (0 != timestamp) {
+                float dt = (event.timestamp - timestamp) * NANO_SECOND_TO_SECOND;
+                float[] radValues = new float[3];
+                radValues[0] = event.values[0] * dt;
+                radValues[1] = event.values[1] * dt;
+                radValues[2] = event.values[2] * dt;
+
+                float newAngelX = (float) Math.toDegrees(radValues[0]);
+                float newAngelY = (float) Math.toDegrees(radValues[1]);
+                float newAngelZ = (float) Math.toDegrees(radValues[2]);
+
+                // dx 仰俯
+                // dy 竖直旋转
+                // dz 水平旋转
+
+                if (0 != angelx || 0 != angely || 0 != angelz) {
+                    float dx = (newAngelX - angelx) * MAGINIFY_FACTOR;
+                    float dy = (newAngelY - angely);
+                    float dz = (newAngelZ - angelz) * MAGINIFY_FACTOR;
+
+                    System.out.println(dy + " " + dz);
+                    // when rotation detected, scroll the content
+                    // UP ROTATION
+                    smoothScrollBy((int) (dy * pixelsPerDegree), 0);
+                    // HORIZONTAL ROTATION
+                    smoothScrollBy((int) (dz * pixelsPerDegree / MAGINIFY_FACTOR), 0);
+                }
+                angelx = newAngelX;
+                angely = newAngelY;
+                angelz = newAngelZ;
+            }
+            timestamp = event.timestamp;
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
--- app/src/main/java/dev/mevur/com/scrollview/MainActivity.java
+++ app/src/main/java/dev/mevur/com/scrollview/MainActivity.java
@@ -1,5 +1,9 @@
 package dev.mevur.com.scrollview;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,16 +11,39 @@ import android.view.View;
 public class MainActivity extends AppCompatActivity {
     private EndlessHorizontalScrollView scrollView;
 
+    private Sensor accelerometer;
+    private Sensor magnetic;
+
+    private SensorManager sensorManager;
+    private Sensor gyroScope;
+
+    private float[] accelerometerValues = new float[3];
+    private float[] magneticFieldValues = new float[3];
+
+    private float north = -11113332;
+
+    private long timestamp;
+
+    private static final float NS2S = 1.0f / 1000000000.0f;
+    private float[] angle = new float[3];
+
+    private float oldAngely = -111111113;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         scrollView = findViewById(R.id.scrollView);
+
     }
 
     public void addView(View view) {
         scrollView.addImg();
     }
+
+
 
 
 }
